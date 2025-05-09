62969726826ca4a9cf91f4d4870f79fa2fc8046c
Hisyam-14 <fadhiilah3837@gmail.com>
2025-05-09 16:30:49 +0800

    first commit

---
+++ assets/footer.php
@@ -0,0 +1,24 @@
+<footer class="footer mt-5 py-3 bg-dark text-white">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-5">
+                <h5>About Us</h5>
+                <p>Selamat datang di Website Resmi Dinas Kehutanan Provinsi Kalimantan Timur. Website ini sebagai sarana publikasi untuk memberikan Informasi dan gambaran tentang Dinas Kehutanan Provinsi Kalimantan Timur dalam melaksanakan pelayanan tugas informasi.</p>
+            </div>
+            <div class="col-md-5 ms-auto">
+                <h5>Quick Links</h5>
+                <ul class="list-unstyled">
+                    <li><a href="index.php" class="text-white">Home</a></li>
+                    <li><a href="products.php" class="text-white">Dashboard</a></li>
+                    <li><a href="contact.php" class="text-white">FastAPI</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 text-center">
+                <hr class="bg-light">
+                <p>&copy; <?php echo date('Y'); ?> Your Company Name. All rights reserved.</p>
+            </div>
+        </div>
+    </div>
+</footer>
